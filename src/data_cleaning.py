d947609cf5e135c2922b3dc6a1b56f8cb807335d
Marypi02 <m.sorrentino2811@gmail.com>
2024-01-28 22:22:12 +0100

    data_cleaning quasi completo; manca la gestione dei dati mancanti per la colonna 'Age'

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -5,23 +5,56 @@ import re
 PATH_SEPARATOR = os.sep
 df=pd.read_excel(f"..{PATH_SEPARATOR}dataset{PATH_SEPARATOR}Dataset.xlsx")
 
-"""Dalla fase di data_understing è sorto che le features 'Age' e 'Monetary'
-presentano dei valori mancanti. In particolare 'Age' in modo considerevole.
-
-Inoltre la colonna 'Name' sembra contenere più informazioni insieme:
+"""Dalla fase di data_understing è sorto che la colonna 'Name' sembra contenere più informazioni insieme:
 cognome, titolo e nome.
+
+Inoltre le features 'Age' e 'Monetary' presentano dei valori mancanti. In particolare 'Age' in modo considerevole (153 e 10 per 'Monetary').
 
 Non meno importante è lo sbilanciamento delle classi!"""
 
-#Divido la colonna Name in due: Surname e Title
-#Definisco una funzione lamba che applica la funzione split() su una stringa e ritorna il primo valore della separazione
-df['Surname'] = df['Name'].apply(lambda x: x.split(',')[0])
-#Check
-#print(df['Surname'])
+# Sostituisco tutti i valori '2' con '0' nella colonna 'Class' per le persone che non hanno donato
+df['Class'] = df['Class'].replace(2, 0)
 
-#In questo caso la labda cerca un pattern definito tramite espressione regolare e se trova un match, restituisce il primo gruppo, ossia quello tra parentesi ()
+# OPERAZIONE SULLE FEATURE
+# Divido la colonna Name in due: Surname e Title
+# Definisco una funzione lamba che applica la funzione split() su una stringa e ritorna il primo valore della separazione
+df['Surname'] = df['Name'].apply(lambda x: x.split(',')[0])
+# Check
+# print(df['Surname'])
+
+# In questo caso la labda cerca un pattern definito tramite espressione regolare e se trova un match, restituisce il primo gruppo, ossia quello tra parentesi ()
 df['Title'] = df['Name'].apply(lambda x: re.search(r',\s*([^\.]+)\.', x).group(1) if re.search(r',\s*([^\.]+)\.', x) else None)
 
-df.to_excel(f"..{PATH_SEPARATOR}dataset{PATH_SEPARATOR}Dataset.xlsx", index=False)
+# Elimino la vecchia colonna
+#df.drop(['Name'], axis=1, inplace=True)
 
-#Visualizzo i risultati ottenuti e quanti valori si ottengono per ciascun titolo
+# Visualizzo i risultati ottenuti e quanti valori si ottengono per ciascun titolo
+print(df['Title'].value_counts())
+
+"""Molti titoli sono ripetuti solo una o due volte, quindi potrebbero non essere molto utili.
+Inoltre, alcune categorie possono essere unite, come Mme sinonimo di Mrs (fanno riferimento a donne sposate)
+e Ms e Mlle sinonimi di Miss (fanno riferimento a donne non sposate)"""
+
+# Itero il dataset per redistribuire le diverse categorie
+for i, row in df.iterrows():
+    title = row['Title']
+    if title == 'Mme':
+        df.loc[i, 'Title'] = 'Mrs'
+    elif title == 'Ms' or title == 'Mlle':
+        df.loc[i, 'Title'] = 'Miss'
+    elif title != 'Mrs' and title != 'Miss' and title != 'Master' and title != 'Mr':
+        df.loc[i, 'Title'] = 'Other'
+
+print(df['Title'].value_counts())
+
+#df.to_excel(f"..{PATH_SEPARATOR}dataset{PATH_SEPARATOR}Dataset.xlsx", index=False)
+
+# GESTIONE DEI DATI MANCANTI
+# I valori mancanti per la colonna 'Monetary' vengono sostituiti attraverso la mediana per ciascuna classe
+medianDonated = df.loc[df['Class'] == 1, 'Monetary'].median()
+medianNotDonated = df.loc[df['Class'] == 0, 'Monetary'].median()
+
+df.loc[(df['Class'] == 1) & (df['Monetary'].isnull()), 'Monetary'] = medianDonated
+df.loc[(df['Class'] == 0) & (df['Monetary'].isnull()), 'Monetary'] = medianNotDonated
+
+df.to_excel(f"..{PATH_SEPARATOR}dataset{PATH_SEPARATOR}DatasetPulito.xlsx", index=False)

d491e6ce2b13a37d9a57e8ef9957840a31d9742c
Marypi02 <m.sorrentino2811@gmail.com>
2024-01-29 22:06:30 +0100

    Terminata la gestione dei valori mancanti; bisogna bilanciare le classi

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -22,11 +22,11 @@ df['Surname'] = df['Name'].apply(lambda x: x.split(',')[0])
 # Check
 # print(df['Surname'])
 
-# In questo caso la labda cerca un pattern definito tramite espressione regolare e se trova un match, restituisce il primo gruppo, ossia quello tra parentesi ()
+# In questo caso la labda cerca un pattern, definito tramite espressione regolare, e se trova un match, restituisce il primo gruppo, ossia quello tra parentesi ()
 df['Title'] = df['Name'].apply(lambda x: re.search(r',\s*([^\.]+)\.', x).group(1) if re.search(r',\s*([^\.]+)\.', x) else None)
 
 # Elimino la vecchia colonna
-#df.drop(['Name'], axis=1, inplace=True)
+df.drop(['Name'], axis=1, inplace=True)
 
 # Visualizzo i risultati ottenuti e quanti valori si ottengono per ciascun titolo
 print(df['Title'].value_counts())
@@ -35,15 +35,20 @@ print(df['Title'].value_counts())
 Inoltre, alcune categorie possono essere unite, come Mme sinonimo di Mrs (fanno riferimento a donne sposate)
 e Ms e Mlle sinonimi di Miss (fanno riferimento a donne non sposate)"""
 
-# Itero il dataset per redistribuire le diverse categorie
-for i, row in df.iterrows():
-    title = row['Title']
-    if title == 'Mme':
-        df.loc[i, 'Title'] = 'Mrs'
-    elif title == 'Ms' or title == 'Mlle':
-        df.loc[i, 'Title'] = 'Miss'
-    elif title != 'Mrs' and title != 'Miss' and title != 'Master' and title != 'Mr':
-        df.loc[i, 'Title'] = 'Other'
+def redistribuzioneCategorie(df):
+    # Itero il dataset per redistribuire le diverse categorie
+    for i, row in df.iterrows():
+        title = row['Title']
+        if title == 'Mme':
+            df.loc[i, 'Title'] = 'Mrs'
+        elif title == 'Ms' or title == 'Mlle':
+            df.loc[i, 'Title'] = 'Miss'
+        elif title != 'Mrs' and title != 'Miss' and title != 'Master' and title != 'Mr':
+            df.loc[i, 'Title'] = 'Other'
+
+    return df
+
+df = redistribuzioneCategorie(df)
 
 print(df['Title'].value_counts())
 
@@ -56,5 +61,12 @@ medianNotDonated = df.loc[df['Class'] == 0, 'Monetary'].median()
 
 df.loc[(df['Class'] == 1) & (df['Monetary'].isnull()), 'Monetary'] = medianDonated
 df.loc[(df['Class'] == 0) & (df['Monetary'].isnull()), 'Monetary'] = medianNotDonated
+
+# I valori mancanti per la colonna 'Age' vengono sostituiti calcolando una mediana delle età delle persone aventi lo stesso titolo
+# medianAgeByTitle è una Serie
+medianAgeByTitle = df.groupby('Title')['Age'].median()
+
+for title, medianAge in medianAgeByTitle.items():
+    df.loc[(df['Title'] == title) & pd.isnull(df['Age']), 'Age'] = medianAge
 
 df.to_excel(f"..{PATH_SEPARATOR}dataset{PATH_SEPARATOR}DatasetPulito.xlsx", index=False)

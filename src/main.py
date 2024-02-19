fe0535bec76b0f10c1a5cb4f065526be161ee4b0
Marypi02 <m.sorrentino2811@gmail.com>
2024-02-19 16:59:52 +0100

    Creazione file a parte per il data_modelling

--- src/main.py
+++ src/main.py
@@ -11,7 +11,7 @@ import data_cleaning as dc
 PATH_SEPARATOR = os.sep
 
 
-dataset = pd.read_excel(f"..{PATH_SEPARATOR}dataset{PATH_SEPARATOR}DatasetFinale.xlsx")
+dataset = pd.read_excel(f"..{PATH_SEPARATOR}dataset{PATH_SEPARATOR}DatasetAgeChange.xlsx")
 #datasetTrue = pd.read_excel(f"..{PATH_SEPARATOR}dataset{PATH_SEPARATOR}DatasetTrue.xlsx")
 
 
@@ -22,29 +22,3 @@ dataset = pd.read_excel(f"..{PATH_SEPARATOR}dataset{PATH_SEPARATOR}DatasetFinale
 
 #dc.changeAgeGroup(dataset)
 #dc.changeCat(dataset)
-
-"""Codifica delle variabili categoriche
-categoricalFeatures = ['Sex', 'AgeGroup']
-
-# Esegui la codifica one-hot per le colonne categoriche
-df_encoded = pd.get_dummies(dataset, columns=categoricalFeatures)
-"""
-
-X = dataset.drop('Class', axis=1)
-y = dataset['Class']
-
-#Suddivisione del dataset in training e test (80%-20%)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-model = DecisionTreeClassifier()
-#Addestramento del modello
-model.fit(X_train, y_train)
-#Esecuzione delle predizioni sul set di dati
-y_pred= model.predict(X_test)
-
-#Valutazione dei risultati
-accuracy = accuracy_score(y_test, y_pred)
-precision = precision_score(y_test, y_pred)
-
-print(f"Accuracy del modello sul test set: {accuracy:.2f}")
-print(f"Precison del modello sul test set: {precision:.2f}")

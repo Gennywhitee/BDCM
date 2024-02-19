fa69f4cf33aa4cc3fe22a01b38d9834ba193ec22
Marypi02 <m.sorrentino2811@gmail.com>
2024-02-19 18:38:44 +0100

    Valutazione del modello

--- src/data_modelling.py
+++ src/data_modelling.py
@@ -1,4 +1,10 @@
 import pandas as pd
+import os
+
+from imblearn.over_sampling import SMOTE
+from imblearn.under_sampling import RandomUnderSampler
+
+PATH_SEPARATOR = os.sep
 
 from sklearn.metrics import accuracy_score, precision_score
 from sklearn.model_selection import train_test_split
@@ -11,9 +17,23 @@ def trainingModello(dataset):
     #Suddivisione del dataset in training e test (80%-20%)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    model = DecisionTreeClassifier()
+    #Salvataggio del dataset di addestramento
+    trainingData = pd.concat([X_train, y_train], axis=1)
+    trainingData.to_excel(f"..{PATH_SEPARATOR}dataset{PATH_SEPARATOR}DatasetTraining.xlsx", index=False)
+
+    #Salvataggio del dataset di test
+    testData = pd.concat([X_test, y_test], axis=1)
+    testData.to_excel(f"..{PATH_SEPARATOR}dataset{PATH_SEPARATOR}DatasetTest.xlsx", index=False)
+
+    #Oversampling&Undersampling
+    X_resampled, y_resampled = oversampling(X_train, y_train)
+
+    #Undersampling
+    X, y = undersampling(X_resampled, y_resampled)
+
+    model = DecisionTreeClassifier(random_state=42)
     #Addestramento del modello
-    model.fit(X_train, y_train)
+    model.fit(X, y)
     #Esecuzione delle predizioni sul set di dati
     y_pred= model.predict(X_test)
 
@@ -25,3 +45,30 @@ def valutazioneModello(y_test, y_pred):
     precision = precision_score(y_test, y_pred)
 
     return accuracy, precision
+
+
+def oversampling(X_train, y_train):
+
+    rapporto_attuale = 0.6
+
+    # Bilanciamento delle classi con SMOTE (tecninca di sovracampionamento)
+    smote = SMOTE(sampling_strategy=rapporto_attuale, random_state=42)
+    X_resampled, y_resampled = smote.fit_resample(X_train, y_train)
+
+    trainingData2 = pd.concat([X_resampled, y_resampled], axis=1)
+    trainingData2.to_excel(f"..{PATH_SEPARATOR}dataset{PATH_SEPARATOR}DatasetTraining2.xlsx", index=False)
+
+    return X_resampled, y_resampled
+
+def undersampling(X_resampled, y_resampled):
+
+    rapporto_attuale = 0.8
+
+    # Bilanciamento delle classi con RandomUnderSampler (tecninca di sottocampionamento)
+    undersamplingData = RandomUnderSampler(sampling_strategy=rapporto_attuale, random_state=42)
+    X, y = undersamplingData.fit_resample(X_resampled, y_resampled)
+
+    trainingData2 = pd.concat([X, y], axis=1)
+    trainingData2.to_excel(f"..{PATH_SEPARATOR}dataset{PATH_SEPARATOR}DatasetTraining3.xlsx", index=False)
+
+    return X, y
--- src/main.py
+++ src/main.py
@@ -1,10 +1,6 @@
 import pandas as pd
 import os
 
-from sklearn.metrics import accuracy_score, precision_score
-from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
-
 import data_understanding as du
 import data_cleaning as dc
 import data_modelling as dm
@@ -12,7 +8,7 @@ import data_modelling as dm
 PATH_SEPARATOR = os.sep
 
 
-dataset = pd.read_excel(f"..{PATH_SEPARATOR}dataset{PATH_SEPARATOR}DatasetFinale.xlsx")
+dataset = pd.read_excel(f"..{PATH_SEPARATOR}dataset{PATH_SEPARATOR}DatasetTraining3.xlsx")
 #datasetTrue = pd.read_excel(f"..{PATH_SEPARATOR}dataset{PATH_SEPARATOR}DatasetTrue.xlsx")
 
 
@@ -24,9 +20,13 @@ dataset = pd.read_excel(f"..{PATH_SEPARATOR}dataset{PATH_SEPARATOR}DatasetFinale
 #dc.changeAgeGroup(dataset)
 #dc.changeCat(dataset)
 
-y_test, y_pred = dm.trainingModello(dataset)
+"""y_test, y_pred = dm.trainingModello(dataset)
 
 accuracy, precision = dm.valutazioneModello(y_test, y_pred)
 
 print(f"Accuracy del modello sul test set: {accuracy:.2f}")
-print(f"Precison del modello sul test set: {precision:.2f}")
+print(f"Precison del modello sul test set: {precision:.2f}")"""
+
+print("Donating blood:", len(dataset[(dataset['Class'] == 1)]))
+#Numero di elementi per la classe 'Not Donating'
+print("Not donating blood:", len(dataset[(dataset['Class'] == 0)]))

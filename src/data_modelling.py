a223846c619a0a6b2009d7034f039f319124daed
Gennywhitee <genny.ambrosio03@gmail.com>
2024-02-20 19:14:54 +0100

    function for data balancing, data-partitioning

--- src/data_modelling.py
+++ src/data_modelling.py
@@ -1,3 +1,5 @@
+import random
+
 import pandas as pd
 import os
 
@@ -6,62 +8,66 @@ from imblearn.under_sampling import RandomUnderSampler
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, precision_score
 from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
 
 PATH_SEPARATOR = os.sep
 
 
-def training_modello(dataset):
-    if dataset is None or not isinstance(dataset, pd.DataFrame):
-        print("Errore: il dataset non è stato inizializzato correttamente o non è un DataFrame.")
-        return None
-
+def data_partitioning(dataset):
     x = dataset.drop('Class', axis=1)
     y = dataset['Class']
 
-    #Suddivisione del dataset in training e test (80%-20%)
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+    # Suddivisione del dataset in training e test (90%-10%)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1, random_state=42)
 
-    """#Salvataggio del dataset di addestramento
-    trainingData = pd.concat([X_train, y_train], axis=1)
-    trainingData.to_excel(f"..{PATH_SEPARATOR}dataset{PATH_SEPARATOR}DatasetTraining.xlsx", index=False)"""
+    # Salvataggio Training dataset
+    trainingData = pd.concat([x_train, y_train], axis=1)
+    trainingData.to_excel(f"..{PATH_SEPARATOR}dataset{PATH_SEPARATOR}DatasetTraining.xlsx", index=False)
 
-    #Salvataggio del dataset di test
+    # Salvataggio del dataset di test
     testData = pd.concat([x_test, y_test], axis=1)
     testData.to_excel(f"..{PATH_SEPARATOR}dataset{PATH_SEPARATOR}DatasetTest.xlsx", index=False)
 
-    #Oversampling&Undersampling per bilanciare la classe
-    x_new_train, y_new_train = bilanciamento_classe(x_train, y_train)
+
+def training_modello(dataset_train, dataset_test):
+    x_train = dataset_train.drop('Class', axis=1)
+    y_train = dataset_train['Class']
+
+    x_test = dataset_test.drop('Class', axis=1)
+    y_test = dataset_test['Class']
 
     model = RandomForestClassifier(random_state=42)
-    #Addestramento del modello
-    model.fit(x_new_train, y_new_train)
-    #Esecuzione delle predizioni sul set di dati
+    # Addestramento del modello
+    model.fit(x_train, y_train)
+    # Esecuzione delle predizioni sul set di dati
     y_pred = model.predict(x_test)
+
+    valutazione_modello(y_test, y_pred)
 
     return y_test, y_pred
 
 
 def valutazione_modello(y_test, y_pred):
-    #Valutazione dei risultati
+    # Valutazione dei risultati
     accuracy = accuracy_score(y_test, y_pred)
     precision = precision_score(y_test, y_pred)
 
-    return accuracy, precision
+    print("--------------PRECISION------------")
+    print(precision)
+    print("--------------ACCURACY------------")
+    print(accuracy)
 
 
-def bilanciamento_classe(x_train, y_train):
+"""def bilanciamento_classe(dataset):
+    # Sovracampionamento
+    datasetOver = oversampling_data(dataset, 200)
 
-    #Sovracampionamento
-    x_resampled, y_resampled = oversampling(x_train, y_train)
-
-    #Sottocampionamento
-    x, y = undersampling(x_resampled, y_resampled)
-
-    return x, y
+    # Sottocampionamento
+    datasetUnder = undersampling(dataset)
+    return x, y"""
 
 
-def oversampling(x_train, y_train):
-
+"""def oversampling(x_train, y_train):
     rapporto_attuale = 0.6
 
     # Bilanciamento delle classi con SMOTE (tecninca di sovracampionamento)
@@ -69,9 +75,12 @@ def oversampling(x_train, y_train):
     x_resampled, y_resampled = smote.fit_resample(x_train, y_train)
 
     return x_resampled, y_resampled
+"""
 
 
-def undersampling(x_resampled, y_resampled):
+def undersampling(dataset):
+    x_resampled = dataset.drop('Class', axis=1)
+    y_resampled = dataset['Class']
 
     rapporto_attuale = 0.8
 
@@ -82,4 +91,55 @@ def undersampling(x_resampled, y_resampled):
     trainingData2 = pd.concat([x, y], axis=1)
     trainingData2.to_excel(f"..{PATH_SEPARATOR}dataset{PATH_SEPARATOR}DatasetTraining.xlsx", index=False)
 
-    return x, y
+    print("Finito Undersampling")
+
+
+def average(dataset):
+    avgFrequency = int(dataset['Frequency'].mean())
+    avgMonetary = int(dataset['Monetary'].mean())
+    avgRecency = int(dataset['Recency'].mean())
+    avgTime = int(dataset['Time'].mean())
+
+    print("MEDIA FREQUENZA: " + str(avgFrequency))
+    print("MEDIA MONETARY: " + str(avgMonetary))
+    print("MEDIA RECENCY: " + str(avgRecency))
+    print("MEDIA TIME: " + str(avgTime))
+
+    avg = [avgFrequency, avgMonetary, avgRecency, avgTime]
+
+    return avg
+
+
+def return_random(x):
+    delta = x // 2  # divisione intera
+    return random.randint(x - delta, x + delta)
+
+
+def oversampling_data(dataset, x):
+    avg = average(dataset)
+    newData = []
+
+    for i in range(x):  # Usa un ciclo for per semplificare il codice
+        new_row = [
+            random.randint(0, 1),  # Sex
+            random.randint(1, 4),  # AgeGroup
+            return_random(avg[0]),  # Frequency
+            return_random(avg[1]),  # Monetary
+            return_random(avg[2]),  # Recency
+            return_random(avg[3]),  # Time
+            0  # Class
+        ]
+        newData.append(new_row)
+
+    # Crea un DataFrame utilizzando i nuovi dati
+    columns = ['Sex', 'AgeGroup', 'Frequency', 'Monetary', 'Recency', 'Time', 'Class']
+    new_data_frame = pd.DataFrame(newData, columns=columns)
+
+    # Salva il DataFrame in un file Excel
+    merge_datasets(dataset, new_data_frame)
+    print("Finito agiunte "+str(x))
+
+
+def merge_datasets(dataset1, dataset2):
+    merged_dataset = pd.concat([dataset1, dataset2], axis=0)
+    merged_dataset.to_excel(f"..{PATH_SEPARATOR}dataset{PATH_SEPARATOR}DatasetTraining.xlsx", index=False)
--- src/main.py
+++ src/main.py
@@ -1,31 +1,26 @@
 import pandas as pd
 import os
 
-import data_understanding as dc
+import data_understanding as du
 import data_cleaning as dc
 import data_modelling as dm
 
 PATH_SEPARATOR = os.sep
 
 #FASE DI DATA CLEANING
+datasetTest = pd.read_excel(f"..{PATH_SEPARATOR}dataset{PATH_SEPARATOR}DatasetTest.xlsx")
+datasetTrain = pd.read_excel(f"..{PATH_SEPARATOR}dataset{PATH_SEPARATOR}DatasetTraining.xlsx")
+datasetFinale = pd.read_excel(f"..{PATH_SEPARATOR}dataset{PATH_SEPARATOR}DatasetFinale.xlsx")
 
-#PRIMO PROCESSING DEI DATI
-df1 = pd.read_excel(f"..{PATH_SEPARATOR}dataset{PATH_SEPARATOR}Dataset.xlsx")
-dataset = (dc.categorizzazione_age(dc.gestione_valori_null_age
-                                   (dc.gestioneValori_null_monetary(dc.redistribuzione_categorie(dc.dividi_name(df1))))))
-dataset.to_excel(f"..{PATH_SEPARATOR}dataset{PATH_SEPARATOR}DatasetPulito.xlsx", index=False,
-                  columns=['Surname', 'Title',	'Sex', 'AgeGroup', 'Frequency', 'Monetary', 'Recency', 'Time',	'Class'])
+#dataset = dc.new_dataset(dc.change_cat(dc.change_age_group(dc.drop_column(df))))
 
-#SECONDO PROCESSING DEI DATI
-df = pd.read_excel(f"..{PATH_SEPARATOR}dataset{PATH_SEPARATOR}DatasetPulito.xlsx")
+#dc.dropColumn(dataset)
+#dc.create_only_true(dataset)
+#dc.showIntersectionAgeAndFrequency(datasetTrue)
+#dc.showIntersectionAgeAndFrequency(datasetFalse)
 
-newDataset = dc.new_dataset(dc.change_cat(dc.change_age_group(dc.drop_column(df))))
-
-#dc.show_intersection_age_and_frequency(datasetTrue)
-#dc.show_intersection_age_and_frequency(datasetFalse)
-
-#FASE DI DATA MODELLING
-y_test, y_pred = dm.training_modello(newDataset)
+#-------------------FASE DI DATA MODELLING---------------------------------
+"""y_test, y_pred = dm.training_modello(dataset)
 
 accuracy, precision = dm.valutazione_modello(y_test, y_pred)
 
@@ -37,4 +32,22 @@ dataf = pd.read_excel(f"..{PATH_SEPARATOR}dataset{PATH_SEPARATOR}DatasetTraining
 print("\n\nSituazione dopo aver bilanciato la classe:")
 print("Donating blood:", len(dataf[(dataf['Class'] == 1)]))
 #Numero di elementi per la classe 'Not Donating'
-print("Not donating blood:", len(dataf[(dataf['Class'] == 0)]))
+print("Not donating blood:", len(dataf[(dataf['Class'] == 0)]))"""
+
+
+# dc.show_intersection_age_and_frequency(dataset)
+# dc.show_intersection_age_and_frequency(datasetFinale)
+
+#du.printFrequency(datasetFinale)
+#dm.merge_datasets(dataset, datasetFinale)
+# dm.average(dataset)
+# dm.oversampling_data(datasetFinale,200)
+
+#dm.data_partitioning(datasetFinale)
+#dm.oversampling_data(datasetTrain,180)
+#dm.undersampling(datasetTrain)
+#du.printFrequency(datasetTest)
+#du.printFrequency(datasetTrain)
+
+#dm.training_modello(datasetTrain, datasetTest)
+
